4a31f1d4c5f77a8bbb332f2588fc5fa54893348d
Elrinth <elrinth@gmail.com>
2020-10-12 01:22:16 +0200

    Bumped stdafx.h to work on new OS.

--- src/stdafx.h
+++ src/stdafx.h
@@ -11,19 +11,19 @@
 // 下で指定された定義の前に対象プラットフォームを指定しなければならない場合、以下の定義を変更してください。
 // 異なるプラットフォームに対応する値に関する最新情報については、MSDN を参照してください。
 #ifndef WINVER				// Windows 95 および Windows NT 4 以降のバージョンに固有の機能の使用を許可します。
-#define WINVER 0x0400		// これを Windows 98 および Windows 2000 またはそれ以降のバージョン向けに適切な値に変更してください。
+#define WINVER 0x600 		// これを Windows 98 および Windows 2000 またはそれ以降のバージョン向けに適切な値に変更してください。
 #endif
 
 #ifndef _WIN32_WINNT		// Windows NT 4 以降のバージョンに固有の機能の使用を許可します。
-#define _WIN32_WINNT 0x0400		// これを Windows 98 および Windows 2000 またはそれ以降のバージョン向けに適切な値に変更してください。
+#define _WIN32_WINNT 0x600 		// これを Windows 98 および Windows 2000 またはそれ以降のバージョン向けに適切な値に変更してください。
 #endif						
 
 #ifndef _WIN32_WINDOWS		// Windows 98 以降のバージョンに固有の機能の使用を許可します。
-#define _WIN32_WINDOWS 0x0410 // これを Windows Me またはそれ以降のバージョン向けに適切な値に変更してください。
+#define _WIN32_WINDOWS 0x600  // これを Windows Me またはそれ以降のバージョン向けに適切な値に変更してください。
 #endif
 
 #ifndef _WIN32_IE			// IE 4.0 以降のバージョンに固有の機能の使用を許可します。
-#define _WIN32_IE 0x0400	// これを IE 5.0  またはそれ以降のバージョン向けに適切な値に変更してください。
+#define _WIN32_IE 0x600 	// これを IE 5.0  またはそれ以降のバージョン向けに適切な値に変更してください。
 #endif
 
 #define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS	// 一部の CString コンストラクタは明示的です。
@@ -39,4 +39,3 @@
 #include <afxcmn.h>			// MFC の Windows コモン コントロール サポート
 #endif // _AFX_NO_AFXCMN_SUPPORT
 #include <afxcview.h>
-
